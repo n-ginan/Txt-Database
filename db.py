72224cad45dee5e56a4bb9307da80cd5e7146f6b
papsykle <toramgarcia@gmail.com>
2025-02-01 21:44:51 +0800

    feat: added autoincrement feature

--- db.py
+++ db.py
@@ -31,7 +31,20 @@ class Database:
 
     @staticmethod
     def write_values(table_name: str, **kwargs):
+        if not Database.has_table(table_name):
+            return "Table does not exist"
         table = Database.get_table(table_name)
+        table_columns = Database.get_columns(table_name)
+        data = ""
+        if Database.has_autoincrement(table_name, Database.get_columns(table_name)[0]):
+            pass
+        for val in kwargs:
+            data += f"{val}, "
+        data = data[:-2]
+        if not len(kwargs) == len(table_columns) and not len(kwargs) == len(table_columns) - 1:
+            return "the amount of columns does not match the amount of data to be written"
+        open(table, "a").write(f"\n{data}")
+        
 
     @staticmethod
     def create_database(db_name: str):
@@ -43,7 +56,7 @@ class Database:
         os.chdir(db_name)
 
     @staticmethod
-    def get_columns(table_name: str):
+    def get_columns(table_name: str) -> list[str]:
         string_column = open(Database.get_table(table_name), "r").readline().rstrip()
         return string_column.split(", ")
 
@@ -54,10 +67,29 @@ class Database:
     @staticmethod
     def has_table(table_name: str):
         return True if f"{table_name}.txt" in os.listdir() else False
+    
+    @staticmethod
+    def has_autoincrement(table_name: str, column_name: str):
+        if AUTOINCREMENT in column_name:
+            with open(table_name, "rb") as read_file:
+                read_file.seek(0, 2)
+                position = read_file.tell()
+                while position > 0:
+                    read_file.seek(position - 1)
+                    if read_file.read(1) == b"\n":
+                        break
+                    position -= 1
+                column = read_file.readline().decode()
+            get_id = ""
+            for char in column:
+                if char == ",":
+                    break
+                get_id += char
+            return int(get_id)
 
 if __name__ == "__main__":
     Database.use_database("this_db")
-    print(Database.get_columns("my_table"))
+    print(Database.has_autoincrement("my_table.txt", "tite_id autoincrement"))
     #print(Database.create_columns("testing", 
     #               tite="testing_id autoincrement pk", 
     #               hotdog="test_name text",
